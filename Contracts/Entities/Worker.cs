dc2d6b774f039e7a6c828fcc16a3436f6ea13183
Silas Salema <silazdasilva20@gmail.com>
2019-12-24 09:39:25 -0300

    Lista de Contratos de Horas

---
+++ Contracts/Entities/Worker.cs
@@ -0,0 +1,51 @@
+﻿using Contracts.Entities.Enums;
+using System.Collections.Generic;
+
+namespace Contracts.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts1 { get; set; }
+
+        public Worker()
+        {
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+            Contracts1 = new List<HourContract>();
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            Contracts1.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts1.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach(HourContract contract in Contracts1)
+            {
+                if(contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+                
+            }
+            return sum;
+        }
+    }
+}
